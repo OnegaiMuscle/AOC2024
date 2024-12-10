d8f91ccb3c565ab5d3ad68b769fd224f3c7e2581
Duc Tai TRAN <tdts2010@gmail.com>
2024-12-10 01:08:39 +0100

    AOC2024

--- scriptb.js
+++ scriptb.js
@@ -4,10 +4,11 @@ const fs = require('fs');
 const data = fs.readFileSync('inputDay07.txt', 'utf8').trim().split('\n');
 const equations = data.map(equation => equation.split(/[: ]+/).map(Number));
 const maxOperators = equations.reduce((max, equation) => Math.max(max, equation.length) , 0) - 2;
-const binaryTable = Array(2 ** maxOperators).fill().map((_,i) => i.toString(2))
+const binaryTable = Array(3 ** maxOperators).fill().map((_,i) => i.toString(3))
 const instructions = [
-  (acc,val) => acc + val,
-  (acc,val) => acc * val,
+  (acc, val) => acc + val,
+  (acc, val) => acc * val,
+  (acc, val) => +(acc + "" + val),
 ];
 function computedata (i, values){
   let operators = values.length - 1;
@@ -25,7 +26,7 @@ function testing(expected, combinations, values)  {
 let sum = equations.reduce( (acc,line) => {
   const [expected, ...values] = line;
   let operators = values.length - 1;
-  let combinations = 2 ** operators;
+  let combinations = 3 ** operators;
   return acc + testing(expected, combinations, values)
   }, 0);
 console.log(sum);
