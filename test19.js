b0d2b190adfdab43c7c16abeeac1629d3d5923c1
Duc Tai TRAN <tdts2010@gmail.com>
2024-12-19 12:58:30 +0100

    AOC2024

--- test19.js
+++ test19.js
@@ -4,23 +4,24 @@ const fs = require('fs');
 const data = fs.readFileSync('inputDay19.txt', 'utf8').trim().split('\n\n');
 const [towelPatterns, designs] = [data[0].split(', '), data[1].split('\n')];
 
-function canCreateDesign(design, patterns) {
-  const dp = Array(design.length + 1).fill(false);
-  dp[0] = true;
+function countDesignCombinations(design, patterns) {
+  const dp = Array(design.length + 1).fill(0);
+  dp[0] = 1;
   for (let i = 1; i <= design.length; i++) {
     for (const pattern of patterns) {
       if (i >= pattern.length && design.slice(i - pattern.length, i) === pattern) {
-        dp[i] = dp[i] || dp[i - pattern.length];
+        dp[i] += dp[i - pattern.length];
       }
     }
   }
   return dp[design.length];
 }
 
-const possibleDesignsCount = designs.reduce((sum, design) => {
-  canCreateDesign(design, towelPatterns) ? sum++ : sum;
+const totalCombinations = designs.reduce((sum, design) => {
+  const combinations = countDesignCombinations(design, towelPatterns)
+  combinations > 0 ? sum += combinations : sum;
   return sum
-}, 0);
-console.log(possibleDesignsCount);
+  }, 0);
+console.log(totalCombinations);
 
 console.timeEnd('Execution Time');
